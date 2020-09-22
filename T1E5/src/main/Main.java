70560539e983862144ba6f8307d9c0bb9e31c811
Raúl Lapeña <Ral Lapea@pdi-rlapena>
2020-09-22 10:19:31 +0200

    Subiendo ejercicio 5 del tema 1

--- T1E5/src/main/Main.java
+++ T1E5/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main {
 
 /*
@@ -8,7 +10,33 @@ public class Main {
  */
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		// Declaramos un array de cadenas
+		String[] cadenas = new String[5];
+		
+		// Herramienta para leer las entradas de teclado
+		Scanner entrada = new Scanner(System.in);
+		
+		// Bucle normal de 0 a 4, rellenando el array de cadenas
+		for(int i = 0; i < 5; i++) {
+			
+			System.out.println("Dame una cadena: ");		
+			cadenas[i] = entrada.nextLine();
+			
+		}
+		
+		// Bucle for each que imprime las cadenas
+		// Para cada elemento de tipo X del array, hacer lo que haya dentro del bucle
+		for(String s : cadenas) {
+			
+			// Herramienta para imprimir por consola
+			System.out.println(s);
+		}
+		
+		
+		entrada.close();
+		
 
 	}
 
